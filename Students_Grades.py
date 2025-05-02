ea2e903399de1da3de4e2fb5ffb415d58499e33b
Esraa-1-hash <Esraa9roka111995@gmail.com>
2025-05-02 11:36:11 +0300

    Add files via upload

---
+++ Students_Grades.py
@@ -0,0 +1,12 @@
+#creat a list fot students' dictionaries
+students_dict=[{"name": "soha","grades": [90, 85, 97 ]},
+               {"name": "zain","grades":[99, 95, 86]},
+               {"name": "mariem","grades":[100, 99, 97]}]
+print("The students and their grades are: ", students_dict )
+st_grades= [{"soha": [90+ 85+ 97]},
+         {"zain": [99+ 95+ 86]},
+         {"mariem": [100+ 99+ 97]}]
+print("The average grade for each student is: ", st_grades)
+
+
+
